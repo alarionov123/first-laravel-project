3bfe526e7a8d3288fdb5c8e0f6809efb1207ef79
alarionov <alarionov@cs-cart.com>
2024-05-05 21:05:18 +0400

    about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,8 @@
-<h1>О блоге</h1>
-<p>Информация о блоге</p>
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+
+@section('content')
+    <h1>О блоге</h1>
+    <p>Информация о блоге</p>
+@endsection
